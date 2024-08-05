8534a17a72024ea2eccb00c9a3d5b2df4c643838
Nithin1297 <nithin.madduri@proclink.com>
2024-08-05 21:00:16 +0530

    total and grand total working

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -5,15 +5,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatChipsModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatChipsModule, MatIconModule,CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  grandTotal: number = 0;
   removeFromCart(item: Iproduct) {
     const idx = this.allItems.indexOf(item);
     return this.allItems.splice(idx, 1);
@@ -23,9 +25,17 @@ export class CartComponent {
   @Input() allItems: Array<Iproduct> = [];
   constructor(public dataService: DataService, private router: Router) {
     this.allItems = dataService.cart;
+    this.calculateGrandTotal();
   }
+
+  calculateGrandTotal() {
+    this.grandTotal = this.allItems.reduce((total, item) => {
+      return total + (parseFloat(item.price) * item.qty);
+    }, 0);
+  }
+
   total() {
-     return
+    return;
   }
 
   ngOnInit() {
