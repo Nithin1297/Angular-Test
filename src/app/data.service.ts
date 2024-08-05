b04f046310df4cef84584f6b3710e92fd0f296a1
Nithin1297 <nithin.madduri@proclink.com>
2024-08-05 18:10:53 +0530

    working on cart qty

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,6 +8,7 @@ export type Iproduct = {
   price: string;
   quantity: string;
   image: string;
+  qty: number;
 };
 
 @Injectable({
@@ -16,7 +17,12 @@ export type Iproduct = {
 export class DataService {
   cart: Array<Iproduct> = [];
   addProductP(item: Iproduct) {
-    this.cart.push(item);
+    if (this.cart.find((i) => item.id == i.id)) {
+      const idx = this.cart.indexOf(item);
+      this.cart[idx].qty += 1;
+    } else {
+      this.cart.push(item);
+    }
   }
   getDataP(): Promise<Iproduct[]> {
     return fetch('https://66b0a87f6a693a95b539a6fd.mockapi.io/Products').then(
