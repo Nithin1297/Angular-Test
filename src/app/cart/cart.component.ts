fb89a2285e4741e86c07263419c87201475fc57c
Nithin1297 <nithin.madduri@proclink.com>
2024-08-05 17:51:34 +0530

    cart refresh

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,20 +1,40 @@
 import { Component, Input } from '@angular/core';
 import { DataService, Iproduct } from '../data.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatChipsModule, MatIconModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  removeFromCart(itemId: string) {
+    return this.allItems.splice(+itemId, 1);
+    this.loaditems();
+  }
+  placeOrder() {
+    return;
+  }
   @Input() allItems: Array<Iproduct> = [];
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, private router: Router) {
     this.allItems = dataService.cart;
   }
   total = 0;
   // for(item of allItems){
   //   this.total += item.price
   // }
+
+  ngOnInit() {
+    this.loaditems();
+  }
+
+  loaditems() {
+    this.router.navigate(['cart']);
+  }
 }
